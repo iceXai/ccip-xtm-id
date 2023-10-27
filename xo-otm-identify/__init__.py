b409685916115faf60383ef8871d8df6237368b1
Stephan Paul <stephan.paul@posteo.net>
2023-10-27 14:23:21 +0200

    [general] added a dunder init

---
+++ xo-otm-identify/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+#coding: utf-8
+"""
+@author: Dr. Stephan Paul (AWI/iceXai; stephan.paul@awi.de)
+"""
+
+# In[] 
+
+from .preproc import CarrierPreProcessor
+from .clap import CommandLineArgParser
