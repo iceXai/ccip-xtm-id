f37970474a2f8fd2e51189b3f13ff13373794829
Stephan Paul <stephan.paul@posteo.net>
2023-10-27 13:33:21 +0200

    [preproc] added a preproc class to be further developed

---
+++ xo-otm-identify/preproc.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+#coding: utf-8
+"""
+@author: Dr. Stephan Paul (AWI/iceXai; stephan.paul@awi.de)
+"""
+
+# In[]
+
+class CarrierPreProcessor:
+    """
+    Class to handle the preprocessing of the given sensor/carrier combo, i.e.,
+    
+    """
+    def __init__(self):
+        self.preproc_rfrnc = None
+        self.preproc_match = None
