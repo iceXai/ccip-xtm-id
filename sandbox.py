2ef4d0d89a08d50919999e7af643a052888c44ba
Stephan Paul <stephan.paul@posteo.net>
2023-10-31 15:41:17 +0100

    [sandbox] started a sandbox file for better testing and toying

---
+++ sandbox.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+#coding: utf-8
+"""
+@author: Dr. Stephan Paul (AWI/iceXai; stephan.paul@awi.de)
+"""
+
+
+# In[]
+
+from xo-otm-identify import CommandLineArgParser
+from xo-otm-identify import PreProcessor
+
