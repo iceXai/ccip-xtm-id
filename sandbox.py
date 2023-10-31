70afb657783839275a50018854070f9ffa2bf160
Stephan Paul <stephan.paul@posteo.net>
2023-10-31 22:29:17 +0100

    [sandbox]  update

--- sandbox.py
+++ sandbox.py
@@ -7,6 +7,17 @@
 
 # In[]
 
-from xo-otm-identify import CommandLineArgParser
-from xo-otm-identify import PreProcessor
+from xo_otm_identify import CommandLineArgParser
+from xo_otm_identify import PreProcessor
+from xo_otm_identify import Configuration
 
+
+# In[]
+
+clarg = CommandLineArgParser()
+#clarg.parse_command_line_arguments()
+clarg.set_test_cfg_arg('otm_envisat-ers2_full_cfg.yaml')
+
+args = clarg.get_args()
+
+cfg = Configuration(args['cfg'])
