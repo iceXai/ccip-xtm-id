3c87a49b5bbd0e06bb68567999e2cde122350e66
Stephan Paul <stephan.paul@posteo.net>
2023-11-02 22:14:21 +0100

    [sandbox] added more code to setup the complete job

--- sandbox.py
+++ sandbox.py
@@ -9,6 +9,7 @@
 
 from xo_otm_identify import CommandLineArgParser
 from xo_otm_identify import PreProcessor
+from xo_otm_identify import Processor
 from xo_otm_identify import Configuration
 
 
@@ -21,3 +22,17 @@ clarg.set_test_cfg_arg('otm_envisat-ers2_full_cfg.yaml')
 args = clarg.get_args()
 
 cfg = Configuration(args['cfg'])
+
+#check override status
+OVERRIDE_PREPROC = cfg.override
+
+for yy, mm in zip(cfg.yy, cfg.mm):
+    #check whether a shapefile already exists
+    SHAPEFILE_EXISTS = cfg.shp_exists(yy, mm)
+    if not SHAPEFILE_EXISTS or SHAPEFILE_EXISTS and OVERRIDE_PREPRO:
+        preproc = PreProcessor(cfg, yy, mm)
+        preproc.run()
+    else:
+        #load it and proc
+        proc = Processor(cfg, yy, mm)
+        
