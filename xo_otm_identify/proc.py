5ef9c81d72239c724fbdda49fa90d6f59ff49d51
Stephan Paul <stephan.paul@posteo.net>
2023-11-02 22:15:32 +0100

    [proc] added a preliminary placeholder

--- xo_otm_identify/proc.py
+++ xo_otm_identify/proc.py
@@ -6,3 +6,26 @@
 
 # In[]
 
+import os
+
+import pandas as pd
+import geopandas as gpd
+import xarray as xr
+import numpy as np
+
+from typing import List, Dict
+from loguru import logger
+
+from .cfg import Configuration
+
+
+
+# In[]
+
+class Processor:
+    """
+    Class to handle the processing of the given sensor/carrier combo, i.e.,
+    the compilation of parameters and waveforms from XOs/OTMs to CSV
+    """
+    def __init__(self, cfg: Configuration, year: str, month: str):
+        pass
