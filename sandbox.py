3a8f92943d3eca1a020bda4c96738973b5e34276
Stephan Paul <stephan.paul@posteo.net>
2023-11-06 16:02:42 +0100

    [sandbox] code update

--- sandbox.py
+++ sandbox.py
@@ -29,15 +29,19 @@ OVERRIDE_PREPROC = cfg.override
 for yy, mm in zip(cfg.yy, cfg.mm):
     #check whether a shapefile already exists
     SHAPEFILE_EXISTS = cfg.shp_exists(yy, mm)
-    if not SHAPEFILE_EXISTS or SHAPEFILE_EXISTS and OVERRIDE_PREPRO:
+    
+    #in case no cossresponding shapefiles exist or the override argument is set
+    #initiate the PreProcessor() class and run it
+    if not SHAPEFILE_EXISTS or SHAPEFILE_EXISTS and OVERRIDE_PREPROC:
         preproc = PreProcessor(cfg, yy, mm)
         #exectutes the preprocssor with compiling the rawdata, identifying 
         #XOs/OTMs, and exporting the geoinfo to shapefiles
         preproc.run()
-    else:
-        #load it and proc
-        proc = Processor(cfg, yy, mm)
-        #exectutes the procesor by importing the geoinfo shapefiles, compiling
-        #the CSV output from identified l1p/l2i files, and exporting it
-        proc.run()
+    
+    #otherwise just run the Processor() class taking care of loading and 
+    #compiling/dumping the CSV output
+    proc = Processor(cfg, yy, mm)
+    #exectutes the procesor by importing the geoinfo shapefiles, compiling
+    #the CSV output from identified l1p/l2i files, and exporting it
+    proc.run()
         
